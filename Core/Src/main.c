86749ac015dd96cf075017228201d0aae949fa62
andreaFaccenda00 <s1119359@studenti.univpm.it>
2025-05-05 00:19:17 +0200

    Adding third semaphore

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -16,6 +16,7 @@
   ******************************************************************************
   */
 /* USER CODE END Header */
+
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
 #include "cmsis_os.h"
@@ -31,35 +32,41 @@
 
 /* Private define ------------------------------------------------------------*/
 /* USER CODE BEGIN PD */
-// semaforo principale (porta A)
-#define RED_PIN        GPIO_PIN_0   // PA0 CN8-A0 GND CN7-8
-#define YELLOW_PIN     GPIO_PIN_1   // PA1 CN8-A1
-#define GREEN_PIN      GPIO_PIN_4   // PA4 CN8-A2
+// Semaforo principale (porta A, CN8)
+#define RED_PIN        GPIO_PIN_0   // PA0 (CN8-A0)
+#define YELLOW_PIN     GPIO_PIN_1   // PA1 (CN8-A1)
+#define GREEN_PIN      GPIO_PIN_4   // PA4 (CN8-A2)
 
-// semaforo secondario (porta C)
-#define RED2_PIN       GPIO_PIN_9   // PC9 CN10-1 GND CN10-9
-#define YELLOW2_PIN    GPIO_PIN_8   // PC8 CN10-2 GND CN10-20
-#define GREEN2_PIN     GPIO_PIN_6   // PC6 CN10-4 GND CN10-32
+// Semaforo secondario (porta C invertito, CN10)
+#define RED2_PIN       GPIO_PIN_9   // PC9 (CN10-1)
+#define YELLOW2_PIN    GPIO_PIN_8   // PC8 (CN10-2)
+#define GREEN2_PIN     GPIO_PIN_6   // PC6 (CN10-4)
 #define SECOND_PORT    GPIOC
 
-// LED ambulanza (porta B)
-#define BLUE_PIN       GPIO_PIN_0   // PB0 CN8-A3
+// Terzo semaforo (porta C, CN7)
+#define RED3_Pin       GPIO_PIN_10  // PC10 (CN7-1)
+#define YELLOW3_Pin    GPIO_PIN_11  // PC11 (CN7-2)
+#define GREEN3_Pin     GPIO_PIN_12  // PC12 (CN7-3)
+#define THIRD_PORT     GPIOC
+
+// LED ambulanza (porta B, CN8-A3)
+#define BLUE_PIN       GPIO_PIN_0   // PB0
 #define BLUE_PORT      GPIOB
 
-// bit per modalità ambulanza
+// Bit per emergenza ambulanza
 #define BIT_AMB_REQ    (1 << 0)
 #define BIT_AMB_DONE   (1 << 1)
 
-// durata fasi in ms
+// Durata fasi in ms
 #define T_RED_MS       4000U
 #define T_YELLOW_MS    1500U
 #define T_GREEN_MS     4000U
 
 typedef struct {
-  TickType_t duration;      // durata in tick
-  uint16_t   setPins;       // GPIO da accendere sul semaforo principale
-  uint16_t   resetPins;     // GPIO da spegnere sul semaforo principale
-  UBaseType_t nextPhase;    // indice della fase successiva
+  TickType_t duration;      // durata fase in tick
+  uint16_t   setPins;       // pin da accendere (primo semaforo)
+  uint16_t   resetPins;     // pin da spegnere (primo semaforo)
+  UBaseType_t nextPhase;    // indice fase successiva
 } Phase_t;
 
 static const Phase_t phases[] = {
@@ -100,16 +107,16 @@ int main(void)
   SystemClock_Config();
   MX_GPIO_Init();
 
-  /* Initialize scheduler */
+  /* Init scheduler */
   osKernelInitialize();
   semaEventGroup = xEventGroupCreate();
 
   /* Create tasks */
   defaultTaskHandle = osThreadNew(StartDefaultTask, NULL, &defaultTask_attributes);
-  xTaskCreate(SemaforoTask,   "Semaforo",   128, NULL, tskIDLE_PRIORITY + 1, NULL);
-  xTaskCreate(AmbulanceTask,  "Ambulanza",  128, NULL, tskIDLE_PRIORITY + 2, NULL);
+  xTaskCreate(SemaforoTask,   "Semaforo",  128, NULL, tskIDLE_PRIORITY+1, NULL);
+  xTaskCreate(AmbulanceTask,  "Ambulanza", 128, NULL, tskIDLE_PRIORITY+2, NULL);
 
-  /* Initialize onboard resources */
+  /* Initialize onboard LED and button */
   BSP_LED_Init(LED_GREEN);
   BSP_PB_Init(BUTTON_USER, BUTTON_MODE_EXTI);
 
@@ -126,7 +133,7 @@ int main(void)
   /* Start scheduler */
   osKernelStart();
 
-  /* Infinite loop */
+  /* We should never reach here */
   while (1) {}
 }
 
@@ -139,24 +146,24 @@ void SystemClock_Config(void)
   RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
 
   HAL_PWREx_ControlVoltageScaling(PWR_REGULATOR_VOLTAGE_SCALE1_BOOST);
-
   RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
-  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+  RCC_OscInitStruct.HSIState       = RCC_HSI_ON;
   RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
-  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
-  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI;
-  RCC_OscInitStruct.PLL.PLLM = RCC_PLLM_DIV4;
-  RCC_OscInitStruct.PLL.PLLN = 85;
-  RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
-  RCC_OscInitStruct.PLL.PLLQ = RCC_PLLQ_DIV2;
-  RCC_OscInitStruct.PLL.PLLR = RCC_PLLR_DIV2;
+  RCC_OscInitStruct.PLL.PLLState   = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource  = RCC_PLLSOURCE_HSI;
+  RCC_OscInitStruct.PLL.PLLM       = RCC_PLLM_DIV4;
+  RCC_OscInitStruct.PLL.PLLN       = 85;
+  RCC_OscInitStruct.PLL.PLLP       = RCC_PLLP_DIV2;
+  RCC_OscInitStruct.PLL.PLLQ       = RCC_PLLQ_DIV2;
+  RCC_OscInitStruct.PLL.PLLR       = RCC_PLLR_DIV2;
   if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK) {
     Error_Handler();
   }
 
-  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK|RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
-  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
-  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.ClockType      = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                                   |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+  RCC_ClkInitStruct.SYSCLKSource   = RCC_SYSCLKSOURCE_PLLCLK;
+  RCC_ClkInitStruct.AHBCLKDivider  = RCC_SYSCLK_DIV1;
   RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
   RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
   if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_4) != HAL_OK) {
@@ -171,110 +178,149 @@ static void MX_GPIO_Init(void)
 {
   GPIO_InitTypeDef GPIO_InitStruct = {0};
 
+  /* Enable GPIO Ports Clock */
   __HAL_RCC_GPIOC_CLK_ENABLE();
   __HAL_RCC_GPIOF_CLK_ENABLE();
   __HAL_RCC_GPIOA_CLK_ENABLE();
   __HAL_RCC_GPIOB_CLK_ENABLE();
 
-  // Reset outputs
+  /* Reset all outputs */
   HAL_GPIO_WritePin(GPIOA, RED_PIN|YELLOW_PIN|GREEN_PIN, GPIO_PIN_RESET);
-  HAL_GPIO_WritePin(GPIOB, BLUE_PIN, GPIO_PIN_RESET);
+  HAL_GPIO_WritePin(BLUE_PORT, BLUE_PIN, GPIO_PIN_RESET);
   HAL_GPIO_WritePin(SECOND_PORT, RED2_PIN|YELLOW2_PIN|GREEN2_PIN, GPIO_PIN_RESET);
+  HAL_GPIO_WritePin(THIRD_PORT, RED3_Pin|YELLOW3_Pin|GREEN3_Pin, GPIO_PIN_RESET);
 
-  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
-  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  /* Configure pins: Semaforo principale (PA0, PA1, PA4) */
+  GPIO_InitStruct.Pin   = RED_PIN|YELLOW_PIN|GREEN_PIN;
+  GPIO_InitStruct.Mode  = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull  = GPIO_NOPULL;
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
-
-  // semaforo principale
-  GPIO_InitStruct.Pin = RED_PIN|YELLOW_PIN|GREEN_PIN;
   HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
 
-  // LED ambulanza
+  /* Configure pin: LED ambulanza (PB0) */
   GPIO_InitStruct.Pin = BLUE_PIN;
-  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+  HAL_GPIO_Init(BLUE_PORT, &GPIO_InitStruct);
 
-  // semaforo secondario
+  /* Configure pins: Semaforo secondario (PC6, PC8, PC9) */
   GPIO_InitStruct.Pin = RED2_PIN|YELLOW2_PIN|GREEN2_PIN;
   HAL_GPIO_Init(SECOND_PORT, &GPIO_InitStruct);
 
-  // USER button
-  GPIO_InitStruct.Pin = B1_Pin;
+  /* Configure pins: Terzo semaforo (PC10, PC11, PC12) */
+  GPIO_InitStruct.Pin = RED3_Pin|YELLOW3_Pin|GREEN3_Pin;
+  HAL_GPIO_Init(THIRD_PORT, &GPIO_InitStruct);
+
+  /* Configure USER button interrupt */
+  GPIO_InitStruct.Pin  = B1_Pin;
   GPIO_InitStruct.Mode = GPIO_MODE_IT_RISING;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
   HAL_GPIO_Init(B1_GPIO_Port, &GPIO_InitStruct);
 
+  /* EXTI interrupt init */
   HAL_NVIC_SetPriority(EXTI15_10_IRQn, 5, 0);
   HAL_NVIC_EnableIRQ(EXTI15_10_IRQn);
 }
 
+/* USER CODE BEGIN 4 */
+/**
+  * @brief Task che gestisce i tre semafori con sincronizzazione del giallo
+  */
 void SemaforoTask(void *argument)
 {
   UBaseType_t phase = 0;
-  TickType_t lastWake = xTaskGetTickCount();
-  for (;;) {
+  TickType_t  lastWake = xTaskGetTickCount();
+
+  for (;;)
+  {
     EventBits_t bits = xEventGroupGetBits(semaEventGroup);
-    if (bits & BIT_AMB_REQ) {
-      // semaforo 1 rosso, semaforo 2 verde
+
+    if (bits & BIT_AMB_REQ)
+    {
+      // Emergenza ambulanza: semaforo1 ROSSO, semaforo2 VERDE, semaforo3 ROSSO
       HAL_GPIO_WritePin(GPIOA, RED_PIN, GPIO_PIN_SET);
       HAL_GPIO_WritePin(GPIOA, YELLOW_PIN|GREEN_PIN, GPIO_PIN_RESET);
       HAL_GPIO_WritePin(SECOND_PORT, RED2_PIN|YELLOW2_PIN|GREEN2_PIN, GPIO_PIN_RESET);
       HAL_GPIO_WritePin(SECOND_PORT, GREEN2_PIN, GPIO_PIN_SET);
+      HAL_GPIO_WritePin(THIRD_PORT, RED3_Pin, GPIO_PIN_SET);
+      HAL_GPIO_WritePin(THIRD_PORT, YELLOW3_Pin|GREEN3_Pin, GPIO_PIN_RESET);
+
+      // Attendi fine ambulanza
       xEventGroupWaitBits(semaEventGroup, BIT_AMB_DONE, pdTRUE, pdTRUE, portMAX_DELAY);
-      phase = 1;
+      phase    = 1;
       lastWake = xTaskGetTickCount();
       continue;
     }
-    // ciclo normale
+
+    // 1) Semaforo principale
     HAL_GPIO_WritePin(GPIOA, phases[phase].setPins, GPIO_PIN_SET);
     HAL_GPIO_WritePin(GPIOA, phases[phase].resetPins, GPIO_PIN_RESET);
-    // secondo semaforo invertito
+
+    // 2) Semaforo secondario (invertito)
     HAL_GPIO_WritePin(SECOND_PORT, RED2_PIN|YELLOW2_PIN|GREEN2_PIN, GPIO_PIN_RESET);
-    if (phases[phase].setPins & RED_PIN) {
-      HAL_GPIO_WritePin(SECOND_PORT, GREEN2_PIN, GPIO_PIN_SET);
-    } else if (phases[phase].setPins & GREEN_PIN) {
-      HAL_GPIO_WritePin(SECOND_PORT, RED2_PIN, GPIO_PIN_SET);
-    } else {
-      HAL_GPIO_WritePin(SECOND_PORT, YELLOW2_PIN, GPIO_PIN_SET);
-    }
+    if      (phases[phase].setPins & RED_PIN)   HAL_GPIO_WritePin(SECOND_PORT, GREEN2_PIN, GPIO_PIN_SET);
+    else if (phases[phase].setPins & GREEN_PIN) HAL_GPIO_WritePin(SECOND_PORT, RED2_PIN,    GPIO_PIN_SET);
+    else                                        HAL_GPIO_WritePin(SECOND_PORT, YELLOW2_PIN, GPIO_PIN_SET);
+
+    // 3) Terzo semaforo (segue il principale, giallo sincronizzato)
+    HAL_GPIO_WritePin(THIRD_PORT, RED3_Pin,    (phases[phase].setPins & RED_PIN)    ? GPIO_PIN_SET : GPIO_PIN_RESET);
+    HAL_GPIO_WritePin(THIRD_PORT, YELLOW3_Pin, (phases[phase].setPins & YELLOW_PIN) ? GPIO_PIN_SET : GPIO_PIN_RESET);
+    HAL_GPIO_WritePin(THIRD_PORT, GREEN3_Pin,  (phases[phase].setPins & GREEN_PIN)  ? GPIO_PIN_SET : GPIO_PIN_RESET);
+
+    // 4) Delay fino alla fase successiva
     vTaskDelayUntil(&lastWake, phases[phase].duration);
     phase = phases[phase].nextPhase;
   }
 }
 
+/**
+  * @brief Task che gestisce la luce blu dell'ambulanza
+  */
 void AmbulanceTask(void *argument)
 {
-  for (;;) {
+  for (;;)
+  {
     xEventGroupWaitBits(semaEventGroup, BIT_AMB_REQ, pdFALSE, pdTRUE, portMAX_DELAY);
+
     HAL_GPIO_WritePin(BLUE_PORT, BLUE_PIN, GPIO_PIN_SET);
     vTaskDelay(pdMS_TO_TICKS(2000));
     HAL_GPIO_WritePin(BLUE_PORT, BLUE_PIN, GPIO_PIN_RESET);
+
     xEventGroupClearBits(semaEventGroup, BIT_AMB_REQ);
     xEventGroupSetBits(semaEventGroup, BIT_AMB_DONE);
   }
 }
 
+/**
+  * @brief Callback chiamato dall'EXTI sul pulsante utente
+  */
 void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin)
 {
-  if (GPIO_Pin == B1_Pin) {
+  if (GPIO_Pin == B1_Pin)
+  {
     BaseType_t xHigherPriorityTaskWoken = pdFALSE;
     xEventGroupSetBitsFromISR(semaEventGroup, BIT_AMB_REQ, &xHigherPriorityTaskWoken);
     portYIELD_FROM_ISR(xHigherPriorityTaskWoken);
   }
 }
 
+/**
+  * @brief Thread di default (blocco minimo)
+  */
 void StartDefaultTask(void *argument)
 {
-  for (;;) {
-    osDelay(1);
-  }
+  for (;;) { osDelay(1); }
 }
 
+/**
+  * @brief Callback TIM6 (tick HAL)
+  */
 void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
 {
-  if (htim->Instance == TIM6) {
-    HAL_IncTick();
-  }
+  if (htim->Instance == TIM6) HAL_IncTick();
 }
 
+/**
+  * @brief Gestione errore
+  */
 void Error_Handler(void)
 {
   __disable_irq();
@@ -282,6 +328,9 @@ void Error_Handler(void)
 }
 
 #ifdef USE_FULL_ASSERT
+/**
+  * @brief Riportai file e riga in caso di assert failure
+  */
 void assert_failed(uint8_t *file, uint32_t line)
 {
 }
